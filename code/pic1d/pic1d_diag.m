b4c27f204bbe40e84d72a7ce442317e85adc0ceb
tokamak1 <48311710+tokamak1@users.noreply.github.com>
2019-04-22 14:13:36 -0400

    Update pic1d_diag.m

--- code/pic1d/pic1d_diag.m
+++ code/pic1d/pic1d_diag.m
@@ -13,7 +13,7 @@ data = [0.1   1.0152   -4.75613E-15
 1.0   2.0459   -0.85133
 1.5   2.63233   -1.77571
 2   3.18914   -2.8272];
-id=2;
+id=4;
 k=data(id,1);
 wr=data(id,2); wi=data(id,3);
 picdat = './pic1d.dat';
@@ -24,21 +24,9 @@ ng = fread(fid,1,'int');
 q = fread(fid,1,'float');
 dx = fread(fid,1,'float');
 dt = fread(fid,1,'float');
-for i = 1 : nt + 1
-    xp = fread(fid,np,'float');
-    vp = fread(fid,np,'float');
-    Eg = fread(fid,ng,'float');
-    Ek(i) = 0.5 * abs(q) * sum(vp.^2); % kinetic energy  
-    Ef(i) = 0.5 * sum(Eg .* Eg) * dx; % potential energy
-    t(i) = (i - 1) * dt;
-    if mod((i - 1), 50) == 0
-        plot(xp,vp,'b.','Markersize',2);
-        xlim([0,ng * dx]);ylim([-6, 6]);
-        title(['Phase space plotting, vp-x, t=',num2str(t(i))]);
-        xlabel('xp');ylabel('vp');
-        drawnow;
-    end
-end
+Ef = fread(fid,nt,'float');
+Ek = fread(fid,nt,'float');
+t = dt : dt : nt * dt;
 %%
 h = figure('Unit','Normalized','position',...
     [0.02 0.4 0.6 0.3],'DefaultAxesFontSize',15);
@@ -51,7 +39,7 @@ legend('boxoff');
 subplot(122);
 
 % Find the corresponding indexes of the extreme max values
-lndE=log(sqrt(real((Ef)))); % EEf=E^2=[exp(gam*t)]^2=exp(2*gam*t)
+lndE=log(sqrt(real((Ef)))); 
 it0=floor(nt*1/20); it1=floor(nt*17/20);
 yy=lndE(it0:it1);
 extrMaxIndex = find(diff(sign(diff(yy)))==-2)+1;
